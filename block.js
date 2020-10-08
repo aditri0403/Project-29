0759cd318ff16c7c51337385de3458caa38bfc2a
aditri0403 <69711402+aditri0403@users.noreply.github.com>
2020-10-08 16:53:26 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,19 @@
+class block{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        fill("yellow");
+        var angle = this.body.angle;
+        rect(this.body.position.x, this.body.position.y,this.width,this.height);
+        rotate(angle);
+    }
+};
